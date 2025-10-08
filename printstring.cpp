4211246e0a967a22845572e000f220476aeb212f
Het7888 <hetgajera7888@gmail.com>
2025-10-08 14:54:48 +0530

    Add files via upload

---
+++ printstring.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+
+    char str[50];
+    printf("enter a string\n");
+    gets(str);
+    void printstr(char[]);
+    printstr(str);
+ return 0;
+}
+void printstr(char s[])
+{
+
+    char*ptr=s;
+    while(*ptr !='\0')
+{
+    printf("%c",*ptr);
+    ptr++;
+}
+}
